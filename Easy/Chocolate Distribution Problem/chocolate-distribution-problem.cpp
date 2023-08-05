6a775c13dc570297802e221c096088ce717df3fb
YASH AGGARWAL <125944406+mr-yash-aggarwal@users.noreply.github.com>
2023-08-05 15:34:51 +0530

    Updated Solution - GfG to GitHub

--- Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
+++ Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
@@ -3,27 +3,18 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
-    public:
- long long findMinDiff(std::vector<long long>& a, long long n, long long m) {
-    if (m == 0 || n == 0)
-        return 0;
 
-    if (n < m)
-        return -1;
-
-    sort(a.begin(), a.end());
-
-    long long min_diff = LLONG_MAX;
-    for (long long i = 0; i + m - 1 < n; i++) {
-        long long diff = a[i + m - 1] - a[i];
-        if (diff < min_diff)
-            min_diff = diff;
-    }
-    return min_diff;
-}  
+class Solution {
+public:
+    long long findMinDiff(vector<long long> a, long long n, long long m) {
+        sort(a.begin(), a.end());
+        long long minDiff = a[m - 1] - a[0];
+        for (int i = 1; i <= n - m; ++i) {
+            minDiff = min(minDiff, a[m + i - 1] - a[i]);
+        }
+        return minDiff;
+    }   
 };
-
 //{ Driver Code Starts.
 int main() {
 	long long t;
