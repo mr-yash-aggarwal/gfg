51007757dd40baa3ac714c4c24f297a3f32045aa
YASH AGGARWAL <125944406+mr-yash-aggarwal@users.noreply.github.com>
2023-11-02 11:19:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Triplet Sum in Array/triplet-sum-in-array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool find3Numbers(int A[], int n, int X)
+    {
+        sort(A,A+n);
+        for(int i=n-1;i>=2;i--)
+        {
+            int l=0,r=i-1;
+            while(l<r)
+            {
+                if(A[i]+A[r]+A[l]==X) return 1;
+                else if(A[i]+A[r]+A[l]>X) r--;
+                else l++;
+            }
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
