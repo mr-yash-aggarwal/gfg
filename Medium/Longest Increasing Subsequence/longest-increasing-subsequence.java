4e0429511504e3f4bf56ecfecded48275deeb619
YASH AGGARWAL <125944406+mr-yash-aggarwal@users.noreply.github.com>
2023-11-02 11:22:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total testcases
+        int t = sc.nextInt();
+        while(t > 0){
+            
+            //taking size of array
+            int n = sc.nextInt();
+            int array[] = new int[n];
+            
+            //inserting elements in the array
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+            
+            //creating an object of class Solution
+            Solution ob = new Solution();
+            
+            //calling longestSubsequence() method of class
+            //Solution
+            System.out.println(ob.longestSubsequence(n,array));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{
+    //Function to find length of longest increasing subsequence.
+    static int longestSubsequence(int size, int a[])
+    {
+        // code here
+        int[] dp = new int[a.length];
+        int len = 0;
+
+        for(int x : a) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if(i < 0) i = -(i + 1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+
+        return len;
+    }
+}  
