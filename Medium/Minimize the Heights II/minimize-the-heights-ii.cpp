74c2583c16cb3e07f56d507c281c983dd857e9ff
YASH AGGARWAL <125944406+mr-yash-aggarwal@users.noreply.github.com>
2023-12-05 00:55:40 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -8,21 +8,18 @@ using namespace std;
 // User function template for C++
 
 class Solution {
-  
   public:
     int getMinDiff(int arr[], int n, int k) {
-        sort(arr, arr+n);
-        int diff = arr[n-1]-arr[0];//assuming no diff is bigger than this
-        for(int i = 1; i<n; i++)
-        {
-            if(arr[i]-k<0) continue;
-            int mini = min(arr[0]+k, arr[i]-k); //trying to make all the towers except first tower to be min
-            int maxi = max(arr[i-1]+k, arr[n-1]-k); //trying to make all the towers except last tower to be max
-            diff = min(diff, maxi-mini);
+        sort(arr, arr + n);
+        int res = arr[n-1] - arr[0];
+        int i = lower_bound(arr, arr + n, k) - arr;
+        for(int j = max(i, 1); j < n; j++) {
+            int mini = min(arr[0] + k, arr[j] - k);
+            int maxi = max(arr[n-1] - k, arr[j-1] + k);
+            res = min(res, maxi - mini);
         }
-        return diff;
+        return res;
     }
-
 };
 
 //{ Driver Code Starts.
